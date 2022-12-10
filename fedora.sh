dab60f793b80836a4a43bc9540377d89c9a63050
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2022-12-10 22:06:53 +0100

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -8,10 +8,10 @@ DNF_PACKAGES=(
   #chromium
   keepassxc                                            # kdbx compatible password manager
   syncthing
-  nemo-seahorse                                        # nemo seahorse integration (sign / encrypt)
+  #nemo-seahorse                                        # nemo seahorse integration (sign / encrypt)
   #sqlitebrowser                                       # simple browser for sqlite databases
   timeshift
-  #remmina remmina-plugins-{vnc,rdp,www,spice,secret}  # remote access
+  remmina remmina-plugins-{vnc,rdp,www,spice,secret}  # remote access
   #squashfs-tools
   #telegram-desktop
   #dislocker                                           # decrypt windows bitlocker volumes
@@ -19,19 +19,29 @@ DNF_PACKAGES=(
   #gparted                                             # graphical partitioning tool
   #grub-customizer                                     # graphical grub configuration
   #audacity
-  vlc                                                  # videolan: vlc media player
+  #vlc                                                  # videolan: vlc media player
   #totem                                               # gnome video player
-  obs-studio
-  shotwell                                             # image viewer
-  #gimp
-  drawing
+  #obs-studio
+  #shotwell                                             # image viewer
+  gimp
+  #drawing
   flameshot                                            # tool to create and modify screenshots
-  #binwalk                                             # tool to analyse binary files for embeded files and executable code
+  binwalk                                             # tool to analyse binary files for embeded files and executable code
   #radare2                                             # reverse engeneering tool (required for cutter)
   #wfuzz                                               # fuzzing tool
-  #gobuster                                            # directory and vhost enumeration
-  #wireshark
+  gobuster                                            # directory and vhost enumeration
+  wireshark
+  nmap
   snapd
+  code
+  anydesk
+  chromium
+  gnome-tweaks
+  gnome-extensions-app
+  texlive-scheme-full
+  texstudio
+  kubernetes-client
+  ansible
 )
 FLATPAK_PACKAGES=(
   #im.riot.Riot                                         # Element Client
@@ -65,9 +75,35 @@ sudo dnf install -y \
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub
 
+# add microsoft repo for vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+cat <<_EOF | sudo tee /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+_EOF
+
+# add repo for anydesk
+sudo rpm --import https://keys.anydesk.com/repos/RPM-GPG-KEY
+cat > /etc/yum.repos.d/AnyDesk-RHEL.repo << "EOF"
+[anydesk]
+name=AnyDesk RHEL - stable
+baseurl=http://rpm.anydesk.com/rhel/$releasever/$basearch/
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://keys.anydesk.com/repos/RPM-GPG-KEY
+EOF
+
 # install additional software
 sudo dnf install -y ${DNF_PACKAGES[@]}
 flatpak install -y ${FLATPAK_PACKAGES[@]}
+
+if which ansible &> /dev/null; then
+  ansible-galaxy collection install community.general vyos.vyos
+fi
 
 snap install ${SNAP_PACKAGES}
 
@@ -82,5 +118,10 @@ echo "PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" |
 #bash scripts/cutter.sh
 
 # switch desktop environment
-sudo dnf install -y cinnamon
-sudo dnf swap -y @gnome-desktop @cinnamon-desktop
+#sudo dnf install -y cinnamon
+#sudo dnf swap -y @gnome-desktop @cinnamon-desktop
+
+# add your user to some groups for applications
+#usermod -aG vboxusers,dialout user
+
+# install jetbrains-toolbox
